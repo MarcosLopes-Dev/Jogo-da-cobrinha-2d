11bc2cde28653470ca80d764b5f378af98a70fcb
Marcos Vinicius Lopes <m.vd2@hotmail.com>
2020-12-14 17:14:38 -0300

    aplicando estilo basico no game

--- jvs/script.js
+++ jvs/script.js
@@ -51,7 +51,7 @@ function iniciarJogo(){
     for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
-            alert('GAME OVER!')
+            alert('GAME OVER!');
         }
     }
 
